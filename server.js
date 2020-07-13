5d629f437b5bcbf3e3e77504185ddfd2d260cc31
Sarnith <61631532+Sarnith@users.noreply.github.com>
2020-07-13 10:47:12 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var Pusher = require('pusher');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// to serve our JavaScript, CSS and index.html
+app.use(express.static('./'));
+
+var pusher = new Pusher({
+  app_id = "1036686"
+key = "04a315eaab1fda938284"
+secret = "fa97ed5cd1ecc1f815d1"
+cluster = "ap2"
+});
+
+app.post('/pusher/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var auth = pusher.authenticate(socketId, channel);
+  res.send(auth);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, () => console.log('Listening at http://localhost:5000'));
