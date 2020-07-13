66fc569b44365dfbdbc596b45903921b8fd8cfe3
Sarnith <61631532+Sarnith@users.noreply.github.com>
2020-07-13 11:02:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // to serve our JavaScript, CSS and index.html
 app.use(express.static('./'));
 
-var pusher = new Pusher({
+var pusher = new Pusher('04a315eaab1fda938284', {
+  cluster: 'ap2'
+});
+/*var pusher = new Pusher({
 app_id = "1036686"
 key = "04a315eaab1fda938284"
 secret = "fa97ed5cd1ecc1f815d1"
 cluster = "ap2"
-});
+});*/
 
 app.post('/pusher/auth', function(req, res) {
   var socketId = req.body.socket_id;
